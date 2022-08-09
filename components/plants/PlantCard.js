e2ba5f064ef03d8c16a3be7ccc037d8ff86ef585
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2022-08-09 15:45:56 -0700

    Update PlantCard.js

--- components/plants/PlantCard.js
+++ components/plants/PlantCard.js
@@ -1,7 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
+const keyStr =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
-const PlantCard = ({ plant, key }) => {
+const triplet = (e1, e2, e3) =>
+  keyStr.charAt(e1 >> 2) +
+  keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+  keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+  keyStr.charAt(e3 & 63);
+
+const rgbDataURL = (r, g, b) =>
+  `data:image/gif;base64,R0lGODlhAQABAPAA${
+    triplet(0, r, g) + triplet(b, 255, 255)
+  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
+
+const PlantCard = ({ plant }) => {
   return (
     <Link href={`/plants/${plant.slug}`} passHref>
       <a>
@@ -13,7 +26,7 @@ const PlantCard = ({ plant, key }) => {
               layout="fill"
               className="overflow-hidden"
               placeholder="blur"
-              priority={key === 0 ? "true" : "false"}
+              blurDataURL={rgbDataURL(192, 192, 192)}
             />
           </div>
           <p className="min-h-[10%] my-4 text-base font-medium text-center transition-colors  2xl:text-lg text-text-dark hover:text-gray-700 ">
